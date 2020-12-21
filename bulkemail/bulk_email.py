b985a3c7b232ea32c5d1a2e28b68debd768c641a
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-21 21:01:30 +1100

    Add ability to email mutliple recipients

--- bulkemail/bulk_email.py
+++ bulkemail/bulk_email.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 from .static_config_parser import StaticConfigParser
+import json
 
 """Bulk email many recipients
 
@@ -10,16 +11,16 @@ This class will send one email to a large group at once.
 class BulkEmail:
     sender_email_address = StaticConfigParser.get('EMAIL', 'email_address')
     sender_email_password = StaticConfigParser.get('EMAIL', 'email_password')
+    recipient_list = json.loads(StaticConfigParser.get('EMAIL', 'recipient_list'))
 
     @classmethod
-    def bulk_email(cls):
-        msg = EmailMessage()
-        msg['Subject'] = 'Hello there'
-        msg['From'] = cls.sender_email_address
-        msg['To'] = cls.sender_email_address
-        msg.set_content('an epic body for an epic email')
-        
+    def bulk_email(cls):   
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(cls.sender_email_address, cls.sender_email_password)
-            smtp.send_message(msg)
-    
+            for recipient in cls.recipient_list:
+                msg = EmailMessage()
+                msg['Subject'] = 'Hello there'
+                msg['From'] = cls.sender_email_address
+                msg['To'] = recipient
+                msg.set_content('an epic body for an epic email')
+                smtp.send_message(msg)
