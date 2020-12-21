e3c75c6f57ccb411390cb25ee280fe4418c1b7d6
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-21 21:24:09 +1100

    Add launch code checking

--- bulkemail/__main__.py
+++ bulkemail/__main__.py
@@ -1,3 +1,11 @@
-import bulkemail.bulk_email
+from bulkemail.bulk_email import BulkEmail
 
-print("hi2")
+launch_code = BulkEmail.generate_launch_code(4)
+user_input = input(f"""You are about to email {BulkEmail.get_recipient_list_length()} people. Are you sure about this?
+Enter the following: {launch_code}
+-> """)
+if user_input != launch_code:  
+    print("Abort mission.")
+    exit()
+
+BulkEmail.bulk_email()
