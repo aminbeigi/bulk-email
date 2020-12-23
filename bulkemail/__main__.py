46e7afffd1f236bb25d4f6e8b9d7195eddd4dd5c
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-23 16:57:24 +1100

    Add if else

--- bulkemail/__main__.py
+++ bulkemail/__main__.py
@@ -6,7 +6,7 @@ from .config_parser import ConfigParser
 logger = Logger.getLogger()
 
 launch_code = BulkEmail.generate_launch_code(4)
-user_input = input(f"""You are about to email {len(BulkEmail.get_recipient_list())} people. Are you sure about this?
+user_input = input(f"""You are about to email {len(BulkEmail.get_recipient_list())} {'people' if len(BulkEmail.get_recipient_list())!=1 else 'persen'}. Are you sure about this?
 Enter the following: {launch_code}
 -> """)
 if user_input != launch_code:  
