3eb138df4dd98f1e9c9e8f2056579732f439879b
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-25 19:21:41 +1100

    Replace single quotes with double

--- bulkemail/__main__.py
+++ bulkemail/__main__.py
@@ -6,15 +6,15 @@ from .config_parser import ConfigParser
 logger = Logger.getLogger()
 
 if len(BulkEmail.get_recipient_list()) == 0:
-    logger.warning('Recipient list is empty. Can\'t send any emails.')
+    logger.warning("Recipient list is empty. Can't send any emails.")
     exit()
 
 launch_code = BulkEmail.generate_launch_code(length=4)
-user_input = input(f"""You are about to email {len(BulkEmail.get_recipient_list())} {'people' if len(BulkEmail.get_recipient_list())!=1 else 'persen'}. Are you sure about this?
+user_input = input(f"""You are about to email {len(BulkEmail.get_recipient_list())} {"people" if len(BulkEmail.get_recipient_list())!=1 else "person"}. Are you sure about this?
 Enter the following: {launch_code}
 -> """)
 if user_input != launch_code:  
-    print('Abort mission.')
+    print("Abort mission.")
     exit()
 
 subject = ConfigParser.config.get('EMAIL', 'Subject')
