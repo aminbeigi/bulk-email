23d9a223c1c65e2963c94f90d2b6754f1919ccaa
Amin Ghasembeigi <me@aminbeigi.com>
2021-08-19 20:55:18 +1000

    Refactor code to take in command line args

--- bulkemail/__main__.py
+++ bulkemail/__main__.py
@@ -1,24 +1,33 @@
 from .bulk_email import BulkEmail
 from .logger import Logger
-from .config_parser import ConfigParser
+import sys
 
-# init logger
-logger = Logger.getLogger()
+if len(sys.argv) != 4:
+    print("Usage: python -m bulkemail bulkemail/recipients.txt bulkemail/subject bulkemail/body")
+    exit(1)
 
-if len(BulkEmail.get_recipient_list()) == 0:
-    logger.warning("Recipient list is empty. Can't send any emails.")
-    exit()
+if __name__ == '__main__':
+    # init logger
+    logger = Logger.getLogger()
+    with open(sys.argv[1], 'r') as f:
+        recipients = [i.strip() for i in f.readlines()] 
+        print(recipients)
+    with open(sys.argv[2], 'r') as f:
+        subject = f.read()
+    with open(sys.argv[3], 'r') as f:
+        body = f.read()
 
-launch_code = BulkEmail.generate_launch_code(length=4)
-user_input = input(f"""You are about to email {len(BulkEmail.get_recipient_list())} {"people" if len(BulkEmail.get_recipient_list())!=1 else "person"}. Are you sure about this?
-Enter the following: {launch_code}
--> """)
-if user_input != launch_code:  
-    print("Abort mission.")
-    exit()
+    if len(recipients) == 0:
+        logger.warning("Recipient list is empty. Can't send any emails.")
+        exit(1)
 
-subject = ConfigParser.config.get('EMAIL', 'Subject')
-body = ConfigParser.config.get('EMAIL', 'body')
+    launch_code = BulkEmail.generate_launch_code(length=4)
+    user_input = input(f"""You are about to email {len(recipients)} {"people" if len(recipients)!=1 else "person"}. Are you sure about this?
+    Enter the following: {launch_code}
+    -> """)
+    if user_input != launch_code:  
+        print("Abort.")
+        exit(0)
 
-BulkEmail.bulk_email(subject, body)
-logger.info(f'{BulkEmail.get_recipient_list()}')
+    BulkEmail.bulk_email(recipients, subject, body)
+    logger.info(f'{recipients}')
--- bulkemail/bulk_email.py
+++ bulkemail/bulk_email.py
@@ -11,16 +11,14 @@ import imghdr
 This class will send one email to a large group at once.
 """
 
-ATTACHMENTS_PATH = 'attachments/'
-
 class BulkEmail:
     sender_email_address = ConfigParser.get('EMAIL', 'email_address')
     sender_email_password = ConfigParser.get('EMAIL', 'email_password')
-    recipient_lst = json.loads(ConfigParser.get('EMAIL', 'recipient_list'))
-    attachment_lst = json.loads(ConfigParser.get('EMAIL', 'attachments'))
+    attachments_path = ConfigParser.get('FILESYSTEM', 'attachments_path')
+    attachments_lst = json.loads(ConfigParser.get('EMAIL', 'attachments'))
 
     @classmethod
-    def bulk_email(cls, subject, body):
+    def bulk_email(cls, recipients, subject, body):
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(cls.sender_email_address, cls.sender_email_password)
             msg = EmailMessage()
@@ -28,20 +26,16 @@ class BulkEmail:
             msg['From'] = cls.sender_email_address
             msg.set_content(body)
 
-            for attachment in cls.attachment_lst:
-                with open(ATTACHMENTS_PATH + attachment, 'rb') as f:
+            for attachment in cls.attachments_lst:
+                with open(f"{cls.attachments_path}/{attachment}", 'rb') as f:
                     file_data = f.read()
                     file_type = imghdr.what(f.name)
                     file_name = f.name
                     msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
-            for recipient in cls.recipient_lst:
+            for recipient in recipients:
                 smtp.send_message(msg=msg, to_addrs=recipient)
     
-    @classmethod
-    def get_recipient_list(cls):
-        return cls.recipient_lst
-
     @staticmethod
     def generate_launch_code(length=4):
         alphabet = string.ascii_uppercase
